e88db7d4bb897c277e78906bc52b7c99287314d2
Tk001111 <114448284+Tk001111@users.noreply.github.com>
2022-10-03 16:15:35 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> def findmax(a, b, c):
+>>> def findMax(a, b, c):
    if a>b:
       biggest=a
    else:
@@ -15,7 +15,7 @@ a=int(input("첫 번째 숫자 입력: "))
 b=int(input("두 번째 숫자 입력: "))
 c=int(input("세 번째 숫자 입력: "))
 
-biggest=findmax(a, b, c)
+biggest=findMax(a, b, c)
 
 print(a, b, c "중 가장 큰 수는 ", biggest, "입니다.")
 SyntaxError: invalid syntax
